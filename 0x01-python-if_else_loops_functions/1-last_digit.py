48a02c70fd73539c7d2a4050dd3b3d3187c1e400
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-11-28 14:11:26 -0800

    Positive or negative, just don.t be last

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,7 @@ last = -last if number < 0 else last
 if last > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last))
 elif (last < 6) and not(last == 0):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
+    print("Last digit of {} is {} and is less than "
+          "6 and not 0".format(number, last))
 else:
     print("Last digit of {} is {} and is 0".format(number, last))
