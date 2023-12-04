5609f2d5dac77c1ec32fd4dd405aafc8e37de0bb
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-04 14:48:03 -0800

    Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+    else:
+        for rows in range(len(matrix)):
+            for col in range(len(matrix[rows])):
+                if col != len(matrix[rows]) - 1:
+                    space= ' '
+                else:
+                    space = ''
+                print("{}".format(matrix[rows][col]), end=space)
+            print()
