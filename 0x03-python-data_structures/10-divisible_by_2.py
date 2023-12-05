98bfae1ce1604c295777e5c2089064244f64a1ba
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-04 16:53:52 -0800

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,5 +3,5 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
         for i in my_list:
-            new_list.append(False if i & 2 else True)
+            new_list.append(False if i % 2 else True)
         return new_list
