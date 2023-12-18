23a3547ae7f9949dcbcc85729af3a54222ca378d
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-18 15:09:03 -0800

    Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
         Return: x elements
     """
     num = 0
-    for i in (0, x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             num += 1
