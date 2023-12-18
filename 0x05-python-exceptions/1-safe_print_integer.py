3eba800ca19f78771dcb15bf68c3b29c625a60cb
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-18 13:51:30 -0800

    Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+        Prints an integer {:d}.format
+        
+        Args:
+            (value)an integer 
+
+        Return:
+            If TypeError or ValueError - False
+            Otherwise - True
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
