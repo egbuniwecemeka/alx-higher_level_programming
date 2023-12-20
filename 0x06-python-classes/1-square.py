2a16f3878a45df2dfb86ffc74741513f7c89f6df
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-12-19 16:29:51 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Square class based on private instance attribute """
+
+class Square:
+    """Square clas """
+
+    def __init__(self, size):
+        """Initializes a new class
+
+        Args:
+            size(int): size of the new square
+        """
+
+        self._size = size
+
+
