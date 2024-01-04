8916d263bdf4d2c060c7d3bfb6c8d6d539707e2a
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-01-04 23:48:21 +0000

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Defines a Rectangle class"""
-class Rectangle:
-	""" A rectangle class"""
-	pass
+"""Defines a Rectangle class."""
 
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
