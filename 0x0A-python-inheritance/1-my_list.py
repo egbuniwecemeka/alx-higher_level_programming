273aa820f517a740f5d1c6181fc03522e8219e81
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-01-08 17:47:07 -0800

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a class that inherits from a list """
+
+class MyList(list):
+    """ Class printing in sorted order from a list """
+
+    def print_sorted(self):
+        """ Prints list in sorted order """
+        print(sorted(self))
