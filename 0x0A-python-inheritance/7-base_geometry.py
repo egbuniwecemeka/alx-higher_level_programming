ccd61cfac4e245671053cde1efc90b8b9f946ad4
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-01-10 15:30:45 -0800

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
 
         """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
