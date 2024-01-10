afebe3615cca6e32b387bdc707cbc9e0626dcbb4
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-01-10 15:12:45 -0800

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" A Base Geometry class integer validator """
+
+
+class BaseGeometry:
+    """ Integer class validation definition
+
+    method:
+    def area - raises error message
+    def integer - validates value
+
+    """
+    def area(self):
+        """ Non - functional method """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates parameters as integers
+
+            Args:
+            name(str): The name of the integer
+            value(int):The value of the parameter
+
+            Raises:
+            TypeError: Returns error if param is not integer
+            ValueError: Returns error if value is less than or 0
+
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
