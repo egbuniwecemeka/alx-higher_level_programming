7ac044b89ef97e36d4132761e37438ec5a57f0cb
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-01-15 17:23:02 -0800

    test_mod fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ A Rectangle clas inheriting from Base"""
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -18,7 +19,10 @@ class Rectangle(Base):
             id(int): identity of Rectangle
 
             Raises:
-
+            TypeError: If either of width or height is not an int
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
         """
         self.width = width
         self.height = height
