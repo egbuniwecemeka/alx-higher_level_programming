7e5b583a752ac9770da6c917531d7141363c0915
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-02-15 05:45:46 -0800

    list all records in a table of a DB

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of a table second_table of a DB of MySQL server
+-- Records of score are arranged in descending order
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
