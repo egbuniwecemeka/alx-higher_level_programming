13d0fde77f12694e317c6cb97898082b0ff2b2b9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-02-18 00:59:49 -0800

    create DB and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- An SQL script that creates a DB hbtn_0d_2 and USER user_0d_2
 -- User should have only SELECT privilege
 -- USER pasword should be user_0d_2_pwd
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2` AND CREATE USER IF NOT EXISTS `user_0d_2` IDENTIFIED BY 'user_0d_pwd'
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`
+CREATE USER IF NOT EXISTS `user_0d_2` IDENTIFIED BY 'user_0d_2_pwd'
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
