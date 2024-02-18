acaa3bb7e9ed1deb761ca0cec593a5515fc736d7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-02-18 01:19:53 -0800

    create DB and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- CREATES a  DB if it dooes not exist
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 -- Creates a user if it does not exist with pasword access
-CREATE USER IF NOT EXISTS `user_0d_2` IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant SELECT privilege on the hbtn_0d_2 DB for user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
