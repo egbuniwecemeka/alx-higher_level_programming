ca149191d36061c0597f054424ad7af479e66b92
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-11 16:44:32 -0700

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length >= 2){
-	console.log(argv[2] + ' is ' + argv[3]);
+if (argv.length >= 2) {
+  console.log(argv[2] + ' is ' + argv[3]);
 }
