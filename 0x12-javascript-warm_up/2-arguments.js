470ac7ca6aaf9fe0c877871a68fb12c98d12c6b8
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-11 15:38:34 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length === 0) { console.log('No argument'); } else if (argv.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (process.argv.length === 0) { console.log('No argument'); } else if (process.argv.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
