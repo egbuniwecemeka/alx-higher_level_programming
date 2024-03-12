8dbf4d3d8a5953b4ddeaf6c385fc16d869d28af0
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-12 16:47:18 -0700

    Esrever5

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const temp = list[len];
+    list[len] = list[i];
+    i++;
+    len--;
+  }
+  return list;
+}
