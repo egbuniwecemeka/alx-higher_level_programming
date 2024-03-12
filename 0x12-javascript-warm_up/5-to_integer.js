c5f9fe4b3abbc733716f57594327ae3007b31ed1
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-11 17:27:45 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const { argv } = require('node:process');
 
 if (isNaN(argv[2]) || argv[2] === undefined) {
-  console.log('No argument');
+  console.log('Not a number');
 } else {
   console.log('My number:', parseInt(argv[2]));
 }
