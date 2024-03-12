6acec72e4db146bc7e8eefd515e52397670dc8b8
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-12 11:50:40 -0700

    Rectangle #1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    this. w = w;
-    this.h = h;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
+
+module.exports = Rectangle
