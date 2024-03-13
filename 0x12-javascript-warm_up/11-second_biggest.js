37c505cba746093d8ed6c5b91fda057ae5398ec6
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-13 16:39:46 -0700

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  const arr = argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) {return b - a}) [1];
+  console.log(second);
+}
