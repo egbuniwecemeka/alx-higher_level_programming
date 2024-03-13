854e70fe2cd6c827c06feadc70323710a4c51662
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-13 08:33:22 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const threeString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 1; i++) {
+  console.log(`${threeString[0]} \n${threeString[1]} \n${threeString[2]}`);
+}
