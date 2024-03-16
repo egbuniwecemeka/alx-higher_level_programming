9a85bff5f2fd9a28ec12e819bd46ff0c21f85536
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-16 09:53:59 -0700

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 import MySQLdb
+import argparse
+
+argparser = argparse.ArgumentParser()
+argv.add_argument("username" , help="mysql username")
+argv = argparser.parse.args()
+
 
 conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
 
@@ -9,3 +15,6 @@ cur = conn.cursor()
 rows = cur.execute("SELECT * FROM states ORDER BY id ASC")
 for row in rows:
     print("(%d, %s)" % (row[0], row[1]))
+
+cur.close()
+conn.close()
