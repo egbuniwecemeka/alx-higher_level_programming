45312a6688e12fe9ba7cd863db42343fcf0fd093
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-16 10:43:51 -0700

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,10 @@
 import MySQLdb
 import sys
 
-if len(sys.argv) >4:
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+if len(sys.argv) >= 4:
+    username = sys.rgv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
