287d1dc49f07bbd273ef174e3818bfd2d47e8a1e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-16 09:39:26 -0700

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,3 +3,9 @@
 import MySQLdb
 
 conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+
+cur = conn.cursor()
+
+rows = cur.execute("SELECT * FROM states ORDER BY id ASC")
+for row in rows:
+    print("(%d, %s)" % (row[0], row[1]))
