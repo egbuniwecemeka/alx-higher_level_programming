78d31981c879179a2dd7830535a1836e5c3ba2d1
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-16 10:27:26 -0700

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,22 +3,21 @@
 import MySQLdb
 import sys
 
-if len(sys.arg) >= 2:
+if len(sys.argv) >= 2:
     username = arg[1]
     password = arg[2]
     database = arg[3]
 
-try:
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
-    cur = conn.cursor()
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+        cur = conn.cursor()
 
-    rows = cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in rows:
-        print(row)
+        rows = cur.execute("SELECT * FROM states ORDER BY id ASC")
+        for row in rows:
+            print(row)
 
     cur.close()
     conn.close()
 except MySQLdb.Error, e:
     print("{}: {}".format(argv[0], argv[1]))
     sys.exit(1)
-
