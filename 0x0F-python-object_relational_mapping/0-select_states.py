60ac813d68776a7a8091bdab82353dcb680460f5
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-16 10:35:57 -0700

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if len(sys.argv) >= 2:
     database = arg[3]
 
     try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
         cur = conn.cursor()
 
         rows = cur.execute("SELECT * FROM states ORDER BY id ASC")
