606cd3e2a7830f96cdef9eff194cda23bb29dae9
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-17 06:06:45 -0700

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 
+    A script that takes an input and matches its value hbtn_0e_0_usa
+    It should be safe from SQL injection
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY '{}'
+                ORDER BY id ASC""".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
