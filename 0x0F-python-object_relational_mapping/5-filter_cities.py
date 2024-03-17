28015e0dec2c90c741a4f6ff34104ed37902cf75
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-17 13:26:11 -0700

    All cities by states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  takes an argument and lists all cities of that state, using the db  """
+
+import MySQLdb
+import sys 
+
+if __name__ == "__main__":
+    db =  MySQLdb.connect(host="localhost", user=sys.argv[1]
+                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    query = """SELECT cities.name FROM %s
+               ORDER BY id ASC """
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
