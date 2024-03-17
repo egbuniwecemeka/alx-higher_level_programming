a636c097593084e4a7ca9f8aa89bc7458bae8dbe
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-17 06:56:09 -0700

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     query = """SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
+                WHERE name LIKE BINARY %s
                 ORDER BY id ASC"""
     cur.execute(query, sys.argv[4])
     rows = cur.fetchall()
