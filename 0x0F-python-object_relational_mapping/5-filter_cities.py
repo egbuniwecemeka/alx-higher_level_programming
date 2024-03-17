ffcf616e7f3a9da955f7037574bd1ded8398c234
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-17 13:30:51 -0700

    All cities by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,11 +2,11 @@
 """  takes an argument and lists all cities of that state, using the db  """
 
 import MySQLdb
-import sys 
+import sys
 
 if __name__ == "__main__":
-    db =  MySQLdb.connect(host="localhost", user=sys.argv[1]
-                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1]
+                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     query = """SELECT cities.name FROM %s
                ORDER BY id ASC """
