0ceaf2523954d697622884c5434213f4f1ad352d
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-17 06:49:14 -0700

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     A script that takes an input and matches its value hbtn_0e_0_usa
     It should be safe from SQL injection
 """
@@ -9,12 +9,13 @@ import sys
 
 
 if __name__ = "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
+    query = """SELECT * FROM states
                 WHERE name LIKE BINARY '{}'
-                ORDER BY id ASC""".format(sys.argv[4]))
+                ORDER BY id ASC"""
+    cur.execute(query, sys.argv[4])
     rows = cur.fetchall()
     for row in rows:
         print(row)
