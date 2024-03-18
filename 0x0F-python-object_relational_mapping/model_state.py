8b1bc849e53abc6f3956174122f43b8db6fd9e91
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-18 05:45:22 +0000

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,11 +9,14 @@ from sqlalchemy import Column, Integer, String
 engine = create_engine('sqlite:///:memory:', echo='True')
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
     
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-engine = create_engine('mysql://root:Cemeka1995.@localhost:3306/hbtn_0e_0_usa', echo=True)
+
+engine = create_engine('mysql://root:Cemeka1995.@localhost:3306/\
+                       hbtn_0e_0_usa', echo=True)
 Base.metadata.create_all(engine)
