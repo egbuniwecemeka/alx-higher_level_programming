b63365b820403dac67d0e5faedede0ab1f2e93a7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-18 16:00:31 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ lists all State objects from the database hbtn_0e_6_usa """
 
-from sqlalchemy import (create engine)
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
