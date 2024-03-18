91384f8465b3f59bb514e18b795a79e00b5f4ecf
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-18 16:01:15 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 import sys
 
 
-if __name__ = '__main__':
+if __name__ = "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadate.create_all(engine)
