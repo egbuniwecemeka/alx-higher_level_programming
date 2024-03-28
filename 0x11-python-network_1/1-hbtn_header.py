e089105abdadb4026a6f3fbdf35d4c12e5ac0050
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-28 16:03:06 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,8 @@ import sys
 import urllib.request
 
 url = sys.argv[1]
-req = urllib.request.Request(url)
+header = {}
+req = urllib.request.Request(url, headers=header)
 with urllib.request.urlopen(req) as response:
     x_req_id = response.getheader('X-Request-Id')
     print(x_req_id)
