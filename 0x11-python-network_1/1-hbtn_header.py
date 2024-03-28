2a517848427f24edb149d45e4cb1356f831b43e1
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-28 14:58:05 -0700

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Python scripts that sends a request to an URL and retrieves Header X-Request-Id value 
+
+"""
+
+import sys
+import urllib.request
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as reponse:
+    x_req_id = response.headers.get('X-Request-Id')
+    print(x_req_id)
