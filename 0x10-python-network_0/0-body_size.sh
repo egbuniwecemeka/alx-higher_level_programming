70804205356e60f142d2950a1614cf76040dea25
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-30 15:17:01 -0700

    curl body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-# Display size in bytes
-
-if [ $# -ne 1 ]; then
-	echo "Useage: $0 <URL>"
-fi
-
-url=$1
-
-body_size=$(curl -s "$1" | wc -c)
-
-echo "$body_size" 
+echo curl -s "$1" | wc -c
