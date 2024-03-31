82a24efd1010f01ef6e9fbfc8ec705b642a97629
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-31 03:14:02 -0700

    curl end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL, sends a request to that URL,
-echo "$(curl -s -w "%{http_code}" "$1" | { read -r status_code; echo "$status_code"; })"
+curl -sL "$1"
