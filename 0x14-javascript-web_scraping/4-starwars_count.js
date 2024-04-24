64e9617568f7f65bf666cb31b9b7fce141edf239
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-04-23 18:26:49 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,22 +6,19 @@ const request = require('request');
 // api url to fetch response from
 const url = process.argv[2];
 
-// Character ID used in filtering result
-const wedge_ant = 18;
-
 request(url, (err, response, body) => {
   // Error handling
   if (err) {
     console.error(err);
   }
-  // 
-  result = JSON.parse(body).result;
+  //
+  const result = JSON.parse(body).result;
   // use reduce to iterate over movies in result array
   console.log(result.reduce((count, movie) => {
     return movie.characters.find((character) => character.endsWith('/18/'))
     // if character ID of 18 is found, increment
-    ? count + 1
+      ? count + 1
     // else display count
-    : count
+      : count;
   }, 0));
 });
