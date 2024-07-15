7cb40d37908e68eb49f4f051e1546a7793ead808
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-15 02:39:02 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@
 
 import sys
 import urllib.request
-if __name__ == __main__:
+if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
