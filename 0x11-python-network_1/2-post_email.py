4de95def858444be77eea2a3f78df58d19da1d1f
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-15 03:41:33 -0700

    Response header value #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
+""" 
+    A python script that takes a URl and email as arguments,
+    passing the email are aprameter to the URL
+"""
 import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-values = {'email': email}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-request = urllib.request.Request(url, data)
-with urllib.request.urlopen(request) as response:
-    html = response.read()
-    print(html.decode('utf8'))
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+        print(html.decode('utf8'))
