f585e237d0d8c76110d5241258d736a49d6e9f38
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-17 06:57:51 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    response = requests.get(url, data={'email': email})
+    response = requests.post(url, data={'email': email})
     print(f'Your email is: {response.text}')
