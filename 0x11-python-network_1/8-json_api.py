026224b3c1874ea49dabfc9585eb2ec35d00db04
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-07-17 09:46:54 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" 
+    A python script that takes a letter as argument,
+    sends a POST request to a URL
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+
+    response = requests.post(url, data={'letter': q})
+
+    try:
+        """ Parse response to JSON """
+        json = response.json()
+        if result:
+            print(f'[{json.get('id')}] {json.get('name')}')
+        else:
+            print('No result')
+    except ValueError:
+        """ if response not a valid JSON """
+        print('Not a valid JSON')
