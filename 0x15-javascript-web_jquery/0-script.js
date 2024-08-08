2521b97e87768c0688e06f4a5079dc41d30ad246
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-07 20:40:00 -0700

    No JQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 // select the <header> element using DOM querySelector
-header = document.querySelector(header);
+const header = document.querySelector('header');
 
 // updates the text color of <header> element to red(#FF0000)
-header.style.color = #FF0000
+header.style.color = '#FF0000';
